d18896c1de53ed8f53ab354457c7db39e33b6693
lukwiya23 <luk23bonnie8@gmail.com>
2019-11-21 18:56:19 +0300

    setting up mailing service

--- program/main.py
+++ program/main.py
@@ -38,3 +38,9 @@ def send_mail():
     body = "Check the amazon link--> https://www.amazon.com/Razer-Blade-15-Smallest-i7-8750H/dp/B07D37VBVD/ref=sr_1_18?crid=1W4ID4KWM5AMP&keywords=razer+laptop&qid=1574347330&s=electronics&sprefix=razor%2Celectronics%2C813&sr=1-18 "
 
     msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        'luk23bonnie8@gmail.com',  # from
+        'luk23bonnie8@gmail.com',  # recipient
+         msg
+    )
