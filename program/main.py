598c727fe410a4196116974ced26e03f48f52bf2
lukwiya23 <luk23bonnie8@gmail.com>
2019-11-21 18:35:37 +0300

    setting up mailing service

--- program/main.py
+++ program/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
  
 URL = "https://www.amazon.com/Razer-Blade-15-Smallest-i7-8750H/dp/B07D37VBVD/ref=sr_1_18?crid=1W4ID4KWM5AMP&keywords=razer+laptop&qid=1574347330&s=electronics&sprefix=razor%2Celectronics%2C813&sr=1-18"
 
@@ -23,5 +24,8 @@ def check_price():
     print(title.strip())
     print(converted_price)
 
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587) #the number is gmail's connection number#
+    
 
 
